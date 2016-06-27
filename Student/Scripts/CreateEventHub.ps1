f9dfbee9a5fb8ceea2d7686d139095a3ed38b77a
Ted Pattison <ted@tedpattison.net>
2016-06-27 08:37:32 +0100

    updates

--- Student/Scripts/CreateEventHub.ps1
+++
@@ -1,7 +0,0 @@
-cls
-$credential = Get-Credential
-Connect-MsolService -Credential $credential
-
-Add-AzureAccount 
-
-New-AzureSBNamespace DeveloperTesting "East US 2" -CreateACSNamespace $true -NamespaceType Messaging
