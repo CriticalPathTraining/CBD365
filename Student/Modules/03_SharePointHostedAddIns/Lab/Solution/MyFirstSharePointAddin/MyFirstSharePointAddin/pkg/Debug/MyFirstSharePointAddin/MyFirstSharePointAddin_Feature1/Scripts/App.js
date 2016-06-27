317cd33e295f0cef05510db309c63af08fb8c045
Ted Pattison <ted@tedpattison.net>
2016-06-27 08:43:46 +0100

    updates

---
+++ Student/Modules/03_SharePointHostedAddIns/Lab/Solution/MyFirstSharePointAddin/MyFirstSharePointAddin/pkg/Debug/MyFirstSharePointAddin/MyFirstSharePointAddin_Feature1/Scripts/App.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+$(onPageLoad);
+
+function onPageLoad() {
+  $("#cmdPressMe").click(onPressMe);
+}
+
+function onPressMe() {
+  $("#content_box").text("Hello SharePoint Add-ins");
+}
