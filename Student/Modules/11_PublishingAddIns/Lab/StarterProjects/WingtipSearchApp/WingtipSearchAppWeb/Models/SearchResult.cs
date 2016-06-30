abffd29e40663c99922d9201a0812d4ff692d9b1
Ted Pattison <ted@tedpattison.net>
2016-06-30 13:36:15 +0100

    update

---
+++ Student/Modules/11_PublishingAddIns/Lab/StarterProjects/WingtipSearchApp/WingtipSearchAppWeb/Models/SearchResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WingtipSearchAppWeb {
+  public class SearchResult {
+    public string Title { get; set; }
+    public string Path { get; set; }
+  }
+}
--- Student/Modules/11_PublishingAddIns/Lab/StarterProjects/WingtipSearchApp/WingtipSearchAppWeb/Properties/AssemblyInfo.cs
+++ Student/Modules/11_PublishingAddIns/Lab/StarterProjects/WingtipSearchApp/WingtipSearchAppWeb/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("WingtipSearchAppWeb")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f3d6b14-eb3d-4a22-bd8c-e42977a46368")]
+[assembly: Guid("63f26f18-679c-42e0-b2b9-146c20956643")]
 
 // Version information for an assembly consists of the following four values:
 //
