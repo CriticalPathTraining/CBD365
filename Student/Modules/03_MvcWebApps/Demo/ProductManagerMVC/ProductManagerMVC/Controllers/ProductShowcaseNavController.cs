5f9f5976c1278bbf1607a2de0d06e935957b6e15
Ted Pattison <ted@tedpattison.net>
2016-12-26 18:07:45 -0500

    MVC Demo Updates

---
+++ Student/Modules/03_MvcWebApps/Demo/ProductManagerMVC/ProductManagerMVC/Controllers/ProductShowcaseNavController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProductManagerMVC.Services;
+
+namespace ProductManagerMVC.Controllers {
+    public class ProductShowcaseNavController : Controller {
+
+        private IProductDataService productService = ProductDataConnector.GetProductDataService();
+
+        public ActionResult Menu() {
+
+            IEnumerable<string> categories = productService.GetAllProducts()
+                .Select(p => p.Category)
+                .Distinct()
+                .OrderBy(cat => cat);
+
+            return View(categories);
+        }
+    }
+}
---
+++ Student/Modules/03_MvcWebApps/Demo/ProductManagerMVC/ProductManagerMVC/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductManagerMVC.Models {
+
+    public class Product {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        [DisplayName("List Price"), DisplayFormat(DataFormatString = "{0:C2}")]
+        public double ListPrice { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+        public string ProductImageUrl { get; set; }
+    }
+
+}
