544c450ab001c7bc6abc51317c175f839e44cf7a
Ted Pattison <ted@tedpattison.net>
2017-01-15 19:56:39 -0500

    Updates

---
+++ Modules/03_MvcWebApps/Lab/Solution/ProductManagerMVC_Exercise2/ProductManagerMVC/Models/IProductDataService.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace ProductManagerMVC.Models {
+
+  public interface IProductDataService {
+    IQueryable<Product> GetAllProducts();
+    Product GetProduct(int id);
+    void AddProduct(Product product);
+    void DeleteProduct(int id);
+    void UpdateProduct(Product product);
+  }
+
+}
+
